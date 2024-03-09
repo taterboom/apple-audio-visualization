50ef9d27282eb47ec7a7c747f9e8c1acf463e86d
xueyong <1085523029@qq.com>
2024-03-09 23:06:19 +0800

    💚 add remotion entry

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        remotion: path.resolve(__dirname, "remotion.html"),
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
